3283f61e8e333e66bc1f459a5fe7488c4ce224a9
user.name <40615590@qq.com>
2019-01-07 14:27:19 +0800

    增加了几个文件

---
+++ loginCheck.php
@@ -0,0 +1,33 @@
+<?php
+	header("Content-type:text/html;charset=utf-8");
+
+	//一、获取用户的输入
+	$phone = $_POST['myphone'];
+	$password = $_POST['passwords'];
+	//二、处理
+
+	//1、建立连接（搭桥）
+	$conn = mysql_connect('localhost','root','root');
+	if(!$conn){
+		die("连接失败");
+	}
+	//2、选择数据库（选择目的地）
+	mysql_select_db("muji",$conn);
+
+	//3、执行SQL语句（传输数据）
+	$sqlstr="select * from login where myphone='$phone' and password='$password'";
+									
+
+	$result = mysql_query($sqlstr,$conn);//结果是个表格
+
+	//4、关闭数据库（过河拆桥）
+	mysql_close($conn);
+
+	//三、响应
+	if(mysql_num_rows($result)>0){
+		echo "1";
+	}else{
+		echo "0";
+	}
+
+?>
---
+++ regSave.php
@@ -0,0 +1,44 @@
+<?php
+	header("Content-type:text/html;charset=utf-8");
+
+	//一、获取用户的输入
+	$myphone = $_POST['myphone'];
+	$verification_code = $_POST['verification_code'];
+
+	//二、处理
+
+	//1、建立连接（搭桥）
+	$conn = mysql_connect('localhost','root','root');
+	if(!$conn){
+		die("连接失败");
+	}
+	//2、选择数据库（选择目的地）
+	mysql_select_db("muji",$conn);
+
+	//3、执行SQL语句（传输数据）
+	//3.1 查询
+	$sqlstr="select * from login where myphone='$myphone' ";
+	$result = mysql_query($sqlstr,$conn);
+
+	if(mysql_num_rows($result)>0){
+		//4、关闭数据库（过河拆桥）
+		mysql_close($conn);
+		
+		echo "该账号已经被使用";
+	}else{
+		$sqlstr="insert into login values('$myphone',$verification_code)";
+		
+
+		$result = mysql_query($sqlstr,$conn);
+
+		//4、关闭数据库（过河拆桥）
+		mysql_close($conn);
+
+		//三、响应
+		if($result==1){
+			echo "1";	
+		}else{
+			echo "0";	
+		}		
+	}
+?>
